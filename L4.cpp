3d62657a4c083deeb68a23af35202fa430032ef6
xiMBest <33019802+xiMBest@users.noreply.github.com>
2017-12-23 22:19:32 +0200

    variables i j -> elementOfRow ElementOfColumn

--- L4.cpp
+++ L4.cpp
@@ -25,12 +25,12 @@ void Matrix::InputMatrix() {
 	cout << "Enter size of matrix>>";
 	cin >> size;
 	array = new int *[size];
-	for (int i = 0; i < size; i++) array[i] = new int[size];
+	for (int elementOfRow = 0; elementOfRow < size; elementOfRow++) array[elementOfRow] = new int[size];
 	cout << "Enter matrix>>\n";
-	for (int i = 0; i < size; i++) {
+	for (int elementOfRow = 0; elementOfRow < size; elementOfRow++) {
 
-		for (int j = 0; j < size; j++) {
-			cin >> array[i][j];
+		for (int elementOfColumn = 0; elementOfColumn < size; elementOfColumn++) {
+			cin >> array[elementOfRow][elementOfColumn];
 
 		}
 	}
@@ -39,10 +39,10 @@ void Matrix::InputMatrix() {
 
 void Matrix::OutputMatrix() {
 
-	for (int i = 0; i < size; i++) {
+	for (int elementOfRow = 0; elementOfRow < size; elementOfRow++) {
 
-		for (int j = 0; j < size; j++) {
-			printf("%5d", array[i][j]);
+		for (int elementOfColumn = 0; elementOfColumn < size; elementOfColumn++) {
+			printf("%5d", array[elementOfRow][elementOfColumn]);
 
 		}
 		cout << endl;
@@ -53,15 +53,15 @@ void Matrix::OutputMatrix() {
 void Matrix::insertionSort()
 {
 	int temp, item;
-	for (int i = 0; i < size; i++)
-		for (int j = 1; j < size; j++) {
+	for (int elementOfRow = 0; elementOfRow < size; elementOfRow++)
+		for (int elemtntOfColumn = 1; elementOfColumn < size; elementOfColumn++) {
 			{
-				temp = array[i][j];
-				item = j - 1;
-				while (item >= 0 && array[i][item] > temp)
+				temp = array[elementOfRow][elementOfColumn];
+				item = elementOfColumn - 1;
+				while (item >= 0 && array[elementOfRow][item] > temp)
 				{
-					array[i][item + 1] = array[i][item];
-					array[i][item] = temp;
+					array[elemntOfRow][item + 1] = array[elementOfRow][item];
+					array[elementOfRow][item] = temp;
 					item--;
 				}
 			}
@@ -71,20 +71,20 @@ void Matrix::MathmeticActions() {
 
 	int sum = 0;
 	long product = 1;
-	int j;
-	for (j = 1; j < size; j++) {
+	int elementOfColumn;
+	for (elementOfColumn = 1; elementOfColumn < size; elementOfColumn++) {
 		sum = 0;
-		for (int i = 0; i < size; i++) {
+		for (int elementOfRow = 0; elementOfRow < size; elementOfRow++) {
 
-			if ((i + j) >= size) {
+			if ((elementOfRow + elementOfColumn) >= size) {
 
-				sum += array[i][j];
+				sum += array[elementOfRow][elementOfColumn];
 			}
 		}
-		cout << " Sum of the " << j + 1 << " column: " << sum << endl;
+		cout << " Sum of the " << elementOfColumn + 1 << " column: " << sum << endl;
 		product *= sum;
 	}
-	float geometricAverage = pow(fabs(product), (1. / j));
+	float geometricAverage = pow(fabs(product), (1. / elementOfColumn));
 
 	cout << "Geometric average = " << geometricAverage << endl;
 
